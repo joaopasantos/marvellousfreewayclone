47474b01c924f6a01f611748d4a9ad4c2876dcf5
João Pedro Albuquerque <joaopasantos0712@gmail.com>
2021-04-03 16:27:52 -0300

    Ajuste no volume dos sons

--- js/sketch.js
+++ js/sketch.js
@@ -4,7 +4,7 @@ var height = 444;
 function setup() {
   createCanvas(640, 444);
 
-  // Inicialização
+  // Inicialização das Arrays de carros
   for (var i = 0; i < (imagemCarros.length/2); i++) {
     xCarro[i] = width;
     yCarro[i] = 50 + (35 * i);
@@ -12,6 +12,13 @@ function setup() {
   for (var i = imagemCarros.length/2; i < (imagemCarros.length); i++) {
     xCarro[i] = 0 - larguraCarro;
     yCarro[i] = 230 + (35 * (i-5));
+  }
+
+  // Ajuste do volume dos sons
+  somGalinha.setVolume(0.05)
+  somPonto.setVolume(0.05)
+  for (var i = 0; i < (somCarros.length); i++) {
+    somCarros[i].setVolume(0.10);
   }
 }
 
